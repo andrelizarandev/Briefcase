84d4906322c6ba339cfb45701ff1c46012965fef
André Lizarán <113467726+andrelizarandev@users.noreply.github.com>
2023-03-01 11:40:45 -0600

    Update index.ts

--- src/data/about-me/index.ts
+++ src/data/about-me/index.ts
@@ -1,1 +1,1 @@
-export const selfDescription = 'Hi, Im Andre Lizaran and Im frontend developer (fullstack developer in process). I love making clean and useful apps/interfaces. My favorite programming language is Typescript. Im from Veracruz, Mexico.'
+export const selfDescription = 'Hi, I am Andre Lizaran and Im frontend developer (fullstack in process). I love making clean and useful apps/interfaces. My favorite programming language is Typescript. Im from Veracruz, Mexico.'
